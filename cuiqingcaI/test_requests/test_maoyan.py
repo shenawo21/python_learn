996a444b8f2f1af0d4329959f51173e01a6102d7
441618375@qq.com <441618375@qq.com>
2018-05-07 15:15:41 +0800

    wxh

--- cuiqingcaI/test_requests/test_maoyan.py
+++ cuiqingcaI/test_requests/test_maoyan.py
@@ -17,7 +17,11 @@ def parse_one_page(html):
     pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
                          + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
                          + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+
+    print(isinstance(html, dict))
+    print(isinstance(html, str))
     items = re.findall(pattern, html)
+    print(items)
     for item in items:
         yield {
             'index': item[0],
@@ -32,14 +36,18 @@ def write_to_file(content):
     with open('result.txt', 'a', encoding='utf-8') as f:
         f.write(json.dumps(content, ensure_ascii=False) + '\n')
 
-def main(offset):
-    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+def main():
+    url = 'http://maoyan.com/board/4'
     html = get_one_page(url)
+    # print(isinstance(html,str))
+    print(1)
     for item in parse_one_page(html):
+        print(2)
         print(item)
         write_to_file(item)
 
 if __name__ == '__main__':
-    for i in range(10):
-        main(offset=i * 10)
-        time.sleep(1)
+    # for i in range(10):
+    #     main(offset=i * 10)
+    #     time.sleep(1)
+    main()
